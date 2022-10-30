3320092b01c877b6538716d31c3e5eaefd4a8e6d
yoichiojima <hmsc.biz41@icloud.com>
2022-10-30 23:20:15 +0900

    schedule the task

---
+++ pipeline/bin/execute_top_50_global.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+cd /app/pipeline
+python3 -m luigi \
+        --module job DownloadGlobalTop50 \
+        --local-scheduler
---
+++ pipeline/bin/schedule_job.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+
+PROJECT=yo-personal-project
+JOB=download-global-top-50
+
+gcloud scheduler jobs create http download-global-top-50 \
+  --location us-central1 \
+  --schedule="0 0 * * *" \
+  --uri="https://us-central1-run.googleapis.com/apis/run.googleapis.com/v1/namespaces/$PROJECT/jobs/$JOB:run" \
+  --http-method POST 
