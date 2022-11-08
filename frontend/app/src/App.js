91b2431c115a3e19f176f0d2445627b631e136de
yoichiojima <hmsc.biz41@icloud.com>
2022-11-08 23:43:41 +0900

    react

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { ChakraProvider, Box, theme } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import MyAccordion from './components/Accordion';
@@ -6,6 +7,19 @@ import { MyTab, MyTab2 } from './components/Tab';
 import { MyAlert } from './components/MyAlert1';
 
 const App = () => {
+  const [data, setData] = useState('');
+
+  const fetchApi = () => {
+    return fetch('http://localhost:8000/')
+      .then(response => response.json())
+      .then(json => {
+        console.log(json.message);
+        setData(json.message);
+      });
+  };
+
+  fetchApi();
+
   return (
     <ChakraProvider theme={theme}>
       <ColorModeSwitcher justifySelf="flex-end" />
@@ -13,6 +27,7 @@ const App = () => {
         <MyAccordion />
         <MyTab />
         <MyTab2 />
+        {data}
         <MyAlert />
       </Box>
     </ChakraProvider>
