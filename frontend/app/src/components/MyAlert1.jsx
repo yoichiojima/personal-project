79e4d4802d813ac04a56b741f8cc1cd0cce2f34b
yoichiojima <hmsc.biz41@icloud.com>
2022-11-08 22:46:19 +0900

    react

--- frontend/app/src/components/MyAlert1.jsx
+++ frontend/app/src/components/MyAlert1.jsx
@@ -1,16 +1,18 @@
 import {
-    Alert,
-    AlertIcon,
-    AlertTitle,
-    AlertDescription,
-  } from '@chakra-ui/react'
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from '@chakra-ui/react';
 
 export const MyAlert = () => {
-    return (
-        <Alert status='error'>
-  <AlertIcon />
-  <AlertTitle>Your browser is outdated!</AlertTitle>
-  <AlertDescription>Your Chakra experience may be degraded.</AlertDescription>
-</Alert>
-    )
-    }
+  return (
+    <Alert status="error">
+      <AlertIcon />
+      <AlertTitle>Your browser is outdated!</AlertTitle>
+      <AlertDescription>
+        Your Chakra experience may be degraded.
+      </AlertDescription>
+    </Alert>
+  );
+};
