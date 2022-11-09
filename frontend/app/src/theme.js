74cc2a105018bade72a3332b6e4b6d81c0fe0593
yoichiojima <hmsc.biz41@icloud.com>
2022-11-09 22:26:22 +0900

    resume

---
+++ frontend/app/src/theme.js
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: `'Noto Sans JP', sans-serif`,
+    body: `'Noto Sans JP', sans-serif`,
+  },
+});
+
+export default theme;
