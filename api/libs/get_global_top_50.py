fbccc804b85ed93c4805490448ab630458921734
yoichiojima <hmsc.biz41@icloud.com>
2022-11-14 03:30:15 +0900

    cache removed

--- api/libs/get_global_top_50.py
+++ api/libs/get_global_top_50.py
@@ -1,15 +1,13 @@
 import sys
 from datetime import datetime
 import pandas as pd
-from _auth.get_authenticated import get_authenticated
+from _auth.google_auth import google_auth
 from _auth.get_project_id import get_project_id
 
 
 def get_global_top_50(_date):
-    get_authenticated()
-    project_id = get_project_id()
-    df = pd.read_csv(f"gs://{project_id}/spotify/global_top_50/{_date}.csv")
-    return df.to_dict('tight')
+    google_auth()
+    return pd.read_csv(f"gs://{get_project_id()}/spotify/global_top_50/{_date}.csv").to_dict('tight')
 
 
 if __name__ == "__main__":
--- libs/_auth/get_authenticated.py
+++
@@ -1,5 +0,0 @@
-import os
-
-
-def get_authenticated():
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/opt/google_auth.json"
