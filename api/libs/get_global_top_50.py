5efe67a27214c90d7eca7e4a049002995026f64f
yoichiojima <hmsc.biz41@icloud.com>
2022-11-14 03:13:51 +0900

    react

--- api/libs/get_global_top_50.py
+++ api/libs/get_global_top_50.py
@@ -5,11 +5,11 @@ from _auth.get_authenticated import get_authenticated
 from _auth.get_project_id import get_project_id
 
 
-def get_global_top_50(_date: datetime):
+def get_global_top_50(_date):
     get_authenticated()
     project_id = get_project_id()
     df = pd.read_csv(f"gs://{project_id}/spotify/global_top_50/{_date}.csv")
-    return list(df["track_name"].values)
+    return df.to_dict('tight')
 
 
 if __name__ == "__main__":
--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import {
   ChakraProvider,
-  Heading,
-  Text,
   theme,
   Container,
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
 } from '@chakra-ui/react';
 
 const App = () => {
@@ -13,10 +20,10 @@ const App = () => {
   useEffect(() => {
     const getGlobalTop50 = async () => {
       const response = await fetch(
-        'http://localhost:8000/global_top_50?date=2022-11-12'
+        'http://localhost:8000/global_top_50?date=2022-11-13'
       );
       const data = await response.json();
-      setGlobalTop50(data);
+      setGlobalTop50(data)
     };
     getGlobalTop50();
   }, []);
@@ -24,10 +31,25 @@ const App = () => {
   return (
     <ChakraProvider theme={theme}>
       <Container>
-        <Heading as="h2">Global Top 50</Heading>
-        {globalTop50.map((artist, key) => (
-          <Text key={key}>{artist}</Text>
-        ))}
+        <TableContainer>
+          <Table variant="simple">
+            <TableCaption>Imperial to metric conversion factors</TableCaption>
+            <Thead>
+              <Tr>
+                {globalTop50.columns && globalTop50.columns.map((column) => (
+                  <Th>{column}</Th>
+                ))}
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                {globalTop50.data.map((item) => (
+                  <Td>{item}</Td>
+                ))}
+              </Tr>
+            </Tbody>
+          </Table>
+        </TableContainer>
       </Container>
     </ChakraProvider>
   );
