4b1f7819dc2f16ce27e3d27f636b61ff01ee0948
yoichiojima <hmsc.biz41@icloud.com>
2022-11-13 23:34:21 +0900

    api

--- api/libs/get_global_top_50.py
+++ api/libs/get_global_top_50.py
@@ -3,23 +3,12 @@ from datetime import datetime
 import pandas as pd
 from _auth.get_authenticated import get_authenticated
 from _auth.get_project_id import get_project_id
-from _storage.list_blobs import list_blobs
 
-def get_global_top_50(_date: datetime) -> dict:
-
+def get_global_top_50(_date: datetime):
     get_authenticated()
-
     project_id = get_project_id()
-
-    df = pd.concat([pd.read_csv(f"gs://{project_id}/{i}") for i in list_blobs(project_id)])
-    df['added_at'] = pd.to_datetime(pd.to_datetime(df['added_at']).dt.tz_localize(None))
-    df['added_at_date'] = pd.to_datetime(df['added_at'].dt.date)
-    df = df[df['added_at_date'] == _date]
-
-    df['added_at'] = df['added_at'].apply(lambda dt: dt.strftime('%Y-%m-%d %H:%M:%S'))
-    df['added_at_date'] = df['added_at_date'].apply(lambda dt: dt.strftime('%Y-%m-%d'))
-    
-    return df.to_dict() 
+    df = pd.read_csv(f"gs://{project_id}/spotify/global_top_50/{_date.strftime('%Y-%m-%d')}.csv")
+    return list(df['track_name'].values)
 
 if __name__ == "__main__":
     print(get_global_top_50(datetime.strptime(sys.argv[1], "%Y-%m-%d")))
--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import {
+  useState, 
+  // useEffect
+} from 'react';
 import {
   ChakraProvider,
   Text, 
@@ -6,10 +10,23 @@ import {
 } from '@chakra-ui/react';
 
 const App = () => {
+  const [globalTop50, setGlobalTop50] = useState({});
+
+  useEffect(() => {
+    const getGlobalTop50 = async () => {
+      const response = await fetch('http://localhost:8000/global_top_50?date=2022-11-12');
+      const data = await response.json();
+      setGlobalTop50(data.track_name);
+    }
+    getGlobalTop50();
+  }, [])
+
+
+  console.log(globalTop50)
+
   return (
     <ChakraProvider theme={theme}>
       <Text>
-        Hello World
       </Text>
     </ChakraProvider>
   );
