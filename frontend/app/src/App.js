576f046dd84bfc6b5b285f489e0630941e0f91c9
yoichiojima <hmsc.biz41@icloud.com>
2022-11-14 02:01:46 +0900

    prettier

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -3,8 +3,8 @@ import {
   ChakraProvider,
   Heading,
   Text,
-  theme, 
-  Container
+  theme,
+  Container,
 } from '@chakra-ui/react';
 
 const App = () => {
@@ -12,20 +12,22 @@ const App = () => {
 
   useEffect(() => {
     const getGlobalTop50 = async () => {
-      const response = await fetch("http://localhost:8000/global_top_50?date=2022-11-12")
-      const data = await response.json()
-      setGlobalTop50(data)
-    }
+      const response = await fetch(
+        'http://localhost:8000/global_top_50?date=2022-11-12'
+      );
+      const data = await response.json();
+      setGlobalTop50(data);
+    };
     getGlobalTop50();
-  }, [])
+  }, []);
 
   return (
     <ChakraProvider theme={theme}>
       <Container>
-      <Heading as='h2'>Global Top 50</Heading>
-      {globalTop50.map((artist, key) => (
-        <Text key={key}>{artist}</Text>
-      ))}
+        <Heading as="h2">Global Top 50</Heading>
+        {globalTop50.map((artist, key) => (
+          <Text key={key}>{artist}</Text>
+        ))}
       </Container>
     </ChakraProvider>
   );
