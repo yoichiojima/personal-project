cbd06f3412e1b3a1f9a4f5deb6b6009266ec0764
yoichiojima <hmsc.biz41@icloud.com>
2022-11-14 23:41:56 +0900

    api

--- frontend/app/src/components/ArtistsAppearedInGlobalTop50.js
+++ frontend/app/src/components/ArtistsAppearedInGlobalTop50.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Container, Text, Image } from '@chakra-ui/react';
+import { Text, Heading, Avatar, HStack, Box } from '@chakra-ui/react';
 
 const ArtistsAppearedInGlobalTop50 = () => {
   const [data, setData] = useState({
@@ -12,29 +12,34 @@ const ArtistsAppearedInGlobalTop50 = () => {
     images: [{ url: '' }],
   });
 
-  console.log(data);
-
   useEffect(() => {
     const getArtistsAppearedInGlobalTop50 = async () => {
       axios
         .get('http://localhost:8000/artists_in_global_top_50/?date=2022-11-14')
         .then(res => {
-          setData(res.data[1]);
-          // console.log(res)
+          setData(res.data[30]);
         });
     };
     getArtistsAppearedInGlobalTop50();
   }, []);
 
   return (
-    <Container>
-      <Text as="h1">{data.name}</Text>
-      <Text>popularity</Text>
-      <Text>{data.popularity}</Text>
-      <Text>followers</Text>
-      <Text>{data.followers.total}</Text>
-      <Image src={data.images[0].url} />
-    </Container>
+    <Box maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden" p={6}>
+      <HStack>
+        <Avatar src={data.images[0].url} size="lg" mr={5} />
+        <Box>
+          <Heading as="h1">{data.name}</Heading>
+          <HStack>
+            <Text as="h2">popularity</Text>
+            <Text as="h2">{data.popularity}</Text>
+          </HStack>
+          <HStack>
+            <Text as="h2">followers</Text>
+            <Text as="h2">{data.followers.total}</Text>
+          </HStack>
+        </Box>
+      </HStack>
+    </Box>
   );
 };
 
