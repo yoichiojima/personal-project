db461f702bd96e7aa067f9e4c7a10ee8d3ee83e2
yoichiojima <hmsc.biz41@icloud.com>
2022-11-14 23:24:33 +0900

    api

--- frontend/app/src/components/ArtistsAppearedInGlobalTop50.js
+++ frontend/app/src/components/ArtistsAppearedInGlobalTop50.js
@@ -1,23 +1,41 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Text } from '@chakra-ui/react';
+import { Container, Text, Image } from '@chakra-ui/react';
 
 const ArtistsAppearedInGlobalTop50 = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState({
+    name: '',
+    popularity: '',
+    followers: {
+      total: '',
+    },
+    images: [{ url: '' }],
+  });
+
+  console.log(data);
 
   useEffect(() => {
     const getArtistsAppearedInGlobalTop50 = async () => {
       axios
-        .get('http://localhost:8000/artists_appeared_in_global_top_50/')
+        .get('http://localhost:8000/artists_in_global_top_50/?date=2022-11-14')
         .then(res => {
-          setData(res.data[0]);
-          console.log(data);
+          setData(res.data[1]);
+          // console.log(res)
         });
     };
     getArtistsAppearedInGlobalTop50();
   }, []);
 
-  return <Text>{data.name}</Text>;
+  return (
+    <Container>
+      <Text as="h1">{data.name}</Text>
+      <Text>popularity</Text>
+      <Text>{data.popularity}</Text>
+      <Text>followers</Text>
+      <Text>{data.followers.total}</Text>
+      <Image src={data.images[0].url} />
+    </Container>
+  );
 };
 
 export default ArtistsAppearedInGlobalTop50;
