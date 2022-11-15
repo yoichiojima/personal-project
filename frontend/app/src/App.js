60cfe3fa5e4babedc907afc354d0ed4fe59df78e
yoichiojima <hmsc.biz41@icloud.com>
2022-11-15 21:18:34 +0900

    react

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,13 +1,21 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
-import { ChakraProvider, theme, Container } from '@chakra-ui/react';
+import {
+  Box,
+  ChakraProvider,
+  theme,
+  Container,
+  Wrap,
+  WrapItem,
+  Heading
+} from '@chakra-ui/react';
 import ArtistsInGlobalTop50 from './components/ArtistsInGlobalTop50';
 
 const App = () => {
   const [artists, setArtists] = useState([
     {
       url: '',
-      name: '',
+      name: 'Artist Name',
       genres: [],
       imageUrl: '',
       popularity: 0,
@@ -20,29 +28,38 @@ const App = () => {
       .get('http://localhost:8000/artists-in-global-top-50/?date=2022-11-14')
       .then(response => {
         setArtists(response.data);
-        console.log(artists.data);
       })
       .catch(error => {
         console.log(error);
       });
+    // eslint-disable-next-line
   }, []);
 
   return (
-    <ChakraProvider theme={theme}>
-      <Container>
-        {artists.map((artist, index) => (
-          <ArtistsInGlobalTop50
-            key={index}
-            url={artist.url}
-            name={artist.name}
-            genres={artist.genres}
-            imageUrl={artist.image_url}
-            popularity={artist.popularity}
-            followers={artist.followers}
-          />
-        ))}
-      </Container>
-    </ChakraProvider>
+    <Box>
+      <ChakraProvider theme={theme}>
+        <Container>
+          <Heading m={4}>
+            Artists in Global Top 50
+          </Heading>
+          <Wrap>
+            {artists.map((artist, index) => (
+              <WrapItem key={index} width={450}>
+                <ArtistsInGlobalTop50
+                  url={artist.url}
+                  name={artist.name}
+                  genres={artist.genres}
+                  imageUrl={artist.image_url}
+                  popularity={artist.popularity}
+                  followers={artist.followers}
+                  index={index}
+                />
+              </WrapItem>
+            ))}
+          </Wrap>
+        </Container>
+      </ChakraProvider>
+    </Box>
   );
 };
 
--- frontend/app/src/components/ArtistsInGlobalTop50.js
+++ frontend/app/src/components/ArtistsInGlobalTop50.js
@@ -11,8 +11,86 @@ import {
   CircularProgressLabel,
   LinkBox,
   LinkOverlay,
-  Wrap,
+  Flex,
+  Container
 } from '@chakra-ui/react';
+
+const ArtistAvatar = ({ imageUrl }) => {
+  return (
+    <Box>
+      <Avatar src={imageUrl} size="xl" />
+    </Box>
+  );
+};
+
+const ArtistHeader = ({ name, popularity, index }) => {
+  const formatter = new Intl.NumberFormat();
+
+  return (
+    <Box overflowX="scroll">
+      <HStack justify="space-between">
+        <Box>
+          <Heading size="lg">
+            {index + 1}. 
+            {name}
+          </Heading>
+        </Box>
+        <Box maxW="20%">
+          <CircularProgress value={popularity} color="green.400">
+            <CircularProgressLabel>
+              {formatter.format(popularity)}
+            </CircularProgressLabel>
+          </CircularProgress>
+        </Box>
+      </HStack>
+    </Box>
+  );
+};
+
+const Genres = ({ genres }) => {
+  return (
+    <Box my={1} width={350}>
+      <Flex display="flex" overflowX="scroll" maxW="100%">
+        {genres.map(genre => (
+          <Box mr={1} key={genre}>
+            <Tag>
+              <Text whiteSpace="noWrap">{genre}</Text>
+            </Tag>
+          </Box>
+        ))}
+      </Flex>
+    </Box>
+  );
+};
+
+const Followers = ({ followers }) => {
+  const formatter = new Intl.NumberFormat();
+
+  return (
+    <Box>
+      <HStack>
+        <Heading size="xs">followers</Heading>
+        <Text>{formatter.format(followers)}</Text>
+      </HStack>
+    </Box>
+  );
+};
+
+const ArtistProfile = ({ name, popularity, genres, followers, index }) => {
+  return (
+    <Container p={4}>
+      <Box mb={3}>
+        <ArtistHeader name={name} popularity={popularity} index={index}/>
+      </Box>
+      <Box mb={4}>
+        <Genres genres={genres} />
+      </Box>
+      <Box mb={2}>
+        <Followers followers={followers} />
+      </Box>
+    </Container>
+  );
+};
 
 const ArtistsInGlobalTop50 = ({
   url,
@@ -21,43 +99,23 @@ const ArtistsInGlobalTop50 = ({
   imageUrl,
   popularity,
   followers,
+  index
 }) => {
-  const formatter = new Intl.NumberFormat();
-
   return (
     <LinkBox>
       <Card>
         <CardBody>
           <HStack>
-            <Avatar src={imageUrl} size="2xl" mr={5} />
-            <Box>
-              <LinkOverlay href={url}>
-                <Heading size="lg" mb={3}>
-                  {name}
-                </Heading>
-              </LinkOverlay>
-              <HStack spacing={4} mb={4}>
-                <Wrap>
-                  {genres.map(genre => (
-                    <Tag key={genre}>{genre}</Tag>
-                  ))}
-                </Wrap>
-              </HStack>
-              <HStack spacing={5}>
-                <HStack>
-                  <Heading size="xs">followers</Heading>
-                  <Text>{formatter.format(followers)}</Text>
-                </HStack>
-                <HStack>
-                  <Heading size="xs">popularity</Heading>
-                  <CircularProgress value={popularity} color="green.400">
-                    <CircularProgressLabel>
-                      {formatter.format(popularity)}
-                    </CircularProgressLabel>
-                  </CircularProgress>
-                </HStack>
-              </HStack>
-            </Box>
+            <LinkOverlay href={url} isExternal>
+              <ArtistAvatar imageUrl={imageUrl} />
+            </LinkOverlay>
+            <ArtistProfile
+              name={name}
+              popularity={popularity}
+              genres={genres}
+              followers={followers}
+              index={index}
+            />
           </HStack>
         </CardBody>
       </Card>
