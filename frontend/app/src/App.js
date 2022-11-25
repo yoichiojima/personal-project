15cbd65f3114461c4a175ada4b0082979f19f5ad
yoichiojima <hmsc.biz41@icloud.com>
2022-11-25 23:02:59 +0900

    react

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,15 +1,54 @@
-import { Box, ChakraProvider, theme, Container } from '@chakra-ui/react';
-import Search from './components/Search';
+import { ChakraProvider, Box, Heading, Text } from "@chakra-ui/react";
+import Chart from "react-apexcharts";
 
 const App = () => {
+  const options = {
+    chart: {
+      id: "basic-bar",
+    },
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+    },
+  };
+  const series = [
+    {
+      name: "series-1",
+      data: [30, 40, 45, 50, 49, 60, 70, 91],
+    },
+  ];
+  const series2 = [44, 55, 41, 17, 15];
+  const options2 = {};
+
   return (
-    <Box>
-      <ChakraProvider theme={theme}>
-        <Container>
-          <Search />
-        </Container>
+    <>
+      <ChakraProvider>
+        <Box>
+          <Heading>Trying Apex Chart</Heading>
+          <Text>Yoichi Ojima</Text>
+          <Box>
+            <Heading>Bar Chart</Heading>
+            <Chart options={options} series={series} type="bar" width="500" />
+          </Box>
+          <Box>
+            <Heading>Line Chart</Heading>
+            <Chart options={options} series={series} type="line" width="500" />
+          </Box>
+          <Box>
+            <Heading>Donut</Heading>
+            <Chart
+              options={options2}
+              series={series2}
+              type="donut"
+              width="380"
+            />
+          </Box>
+          <Box>
+            <Heading>Area Chart</Heading>
+            <Chart options={options} series={series} type="area" width="500" />
+          </Box>
+        </Box>
       </ChakraProvider>
-    </Box>
+    </>
   );
 };
 
