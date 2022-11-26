e27fb749c36194cf6c67c358a5232a6bf9703ac7
yoichiojima <hmsc.biz41@icloud.com>
2022-11-26 20:51:41 +0900

    apex

--- playground/apexchart-example/backend/libs/apex_chart.py
+++ playground/apexchart-example/backend/libs/apex_chart.py
@@ -8,25 +8,25 @@ class ApexChart:
     def _set_id(self, id: str):
         self.res["options"]["chart"]["id"] = id
 
-    def _set_xaxis_categories(self, categories: list):
-        self.res["options"]["xaxis"]["categories"] = categories
-
-    def _set_xaxis_type(self, type: str):
-        self.res["options"]["xaxis"]["type"] = type
-
     def _set_background(self, background: str):
         self.res["options"]["chart"]["background"] = background
 
     def _set_forecolor(self, forecolor: str):
         self.res["options"]["chart"]["foreColor"] = forecolor
 
+    def _set_xaxis_type(self, type: str):
+        self.res["options"]["xaxis"]["type"] = type
+
+    def _set_xaxis_categories(self, categories: list):
+        self.res["options"]["xaxis"]["categories"] = categories
+
     def set_options(
         self,
         id: str = None,
-        categories: list = None,
-        type: str = None,
         background: str = None,
         fore_color: str = None,
+        type: str = None,
+        categories: list = None,
     ):
         if id:
             self._set_id(id)
