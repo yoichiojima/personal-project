475bbb4892e1af2830f743f3f9c4874b1eeab0b2
yoichiojima <hmsc.biz41@icloud.com>
2022-12-06 16:20:47 +0900

    react

---
+++ playground/LeetCode/TwoSum.py
@@ -0,0 +1,3 @@
+class Solution:
+    def twoSum():
+        pass    
---
+++ playground/LeetCode/test_twosum.py
@@ -0,0 +1,7 @@
+from TwoSum import Solution
+
+def test_twosum():
+    s = Solution()
+    assert s.twoSum([2, 7, 11, 15], 9) == [0, 1]
+    assert s.twoSum([3, 2, 4], 6) == [1, 2]
+    assert s.twoSum([3, 3], 6) == [0, 1]
